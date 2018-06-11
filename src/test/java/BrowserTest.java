b448b2d09a817044662efe394e37f0a24a976a09
Tatsiana_Razanava <tatsiana_razanava@epam.com>
2018-06-11 21:00:26 +0300

    added: verifyLogin

--- src/test/java/BrowserTest.java
+++ src/test/java/BrowserTest.java
@@ -1,18 +1,33 @@
+import org.openqa.selenium.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 
 public class BrowserTest {
-
     private WebDriver driver;
 
-    @Test
-    public void testBrowser() {
+    private static final String URL = "https://test-auto2-15.corp.globoforce.com/microsites/t/home?client=testclient5015&setCAG=true";
+    private String userName = "norma_nominator";
+    private String password = "norma_nominator1";
+    private String recipient = "Adam Admin";
+
+    @BeforeClass
+    public void startDriver() {
         String val = System.getProperty("webdriver.browser");
         System.out.println("Browser in use: " + val);
-        System.setProperty("webdriver." + val + ".driver", "src/test/resources/" + val + "driver");
+        System.setProperty("webdriver." + val + ".driver", "src/test/resources/" + val + "driver.exe");
 
         switch (val) {
             case "chrome":
@@ -25,8 +40,32 @@ public class BrowserTest {
                 Assert.fail("Invalid webdriver: " + val);
                 break;
         }
-
-        driver.navigate().to("http://google.com");
     }
 
+    @Test
+    public void verifyLogin() {
+        driver.navigate().to(String.format(URL));
+        driver.findElement(By.name("username")).sendKeys(userName);
+        driver.findElement(By.name("password")).sendKeys(password);
+        driver.findElement(By.id("signIn-button")).click();
+        Assert.assertTrue(driver.findElements(By.xpath("//*[@class='relative']//*[contains(text(), 'Norma Nominator')]")).size() > 0, "Login failed");
+    }
+
+    @Test
+    public void verifyNomination(){
+        driver.findElement(By.id("giveAward-button")).click();
+        driver.findElement(By.id("np-recipient-search-field")).sendKeys(recipient);
+        //driver.findElement(By.xpath("//*[@id=\"np-recipient-search-field\"]")).isEnabled();
+
+    }
+
+
+//    @AfterClass(alwaysRun = true)
+//    public void closeDriver() {
+//        if (driver != null)
+//            driver.quit();
+//    }
+
+
 }
+
